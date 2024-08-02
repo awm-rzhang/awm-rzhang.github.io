b9d21edda174b7acf37ca1f0f9fe9b8fd1614d5b
Ryan Zhang <rzhang@awm.tech>
2024-08-02 10:47:00 -0700

    update description

--- src/script/pages/app-home.ts
+++ src/script/pages/app-home.ts
@@ -105,71 +105,23 @@ export class AppHome extends LitElement {
       <app-header></app-header>
       <div>
         <div id="welcomeBar">
-          <fluent-card id="welcomeCard">
-            <h2>${this.message}</h2>
-
-            <p>
-              For more information on the PWABuilder pwa-starter, check out the
-              <fluent-anchor
-                href="https://github.com/pwa-builder/pwa-starter/wiki/Getting-Started"
-                appearance="hypertext"
-                >Documentation on Github</fluent-anchor
-              >.
-            </p>
-
-            <p id="mainInfo">
-              Welcome to the
-              <fluent-anchor href="https://pwabuilder.com" appearance="hypertext"
-                >PWABuilder</fluent-anchor
-              >
-              pwa-starter! Be sure to head back to
-              <fluent-anchor href="https://pwabuilder.com" appearance="hypertext"
-                >PWABuilder</fluent-anchor
-              >
-              when you are ready to ship this PWA to the Microsoft Store, Google Play
-              and the Apple App Store!
-            </p>
-
-            ${'share' in navigator
-              ? html`<fluent-button appearance="primary" @click="${this.share}"
-                  >Share this Starter!</fluent-button
-                >`
-              : null}
-          </fluent-card>
-
           <fluent-card id="infoCard">
-            <h2>Technology Used</h2>
+            <h2>Features</h2>
 
             <ul>
               <li>
                 <fluent-anchor
-                  href="https://www.typescriptlang.org/"
+                  href="/cra-pwa"
                   appearance="hypertext"
-                  >TypeScript</fluent-anchor
+                  >PWA example</fluent-anchor
                 >
               </li>
 
               <li>
                 <fluent-anchor
-                  href="https://lit.dev"
+                  href="/react-lab"
                   appearance="hypertext"
-                  >lit</fluent-anchor
-                >
-              </li>
-
-              <li>
-                <fluent-anchor
-                  href="https://docs.microsoft.com/en-us/fluent-ui/web-components/"
-                  appearance="hypertext"
-                  >Fluent Web Components</fluent-anchor
-                >
-              </li>
-
-              <li>
-                <fluent-anchor
-                  href="https://vaadin.github.io/vaadin-router/vaadin-router/demo/#vaadin-router-getting-started-demos"
-                  appearance="hypertext"
-                  >Vaadin Router</fluent-anchor
+                  >Ryanz Space</fluent-anchor
                 >
               </li>
             </ul>
@@ -178,7 +130,7 @@ export class AppHome extends LitElement {
           <fluent-anchor href="${(import.meta as any).env.BASE_URL}about" appearance="accent">Navigate to About</fluent-anchor>
         </div>
 
-        <pwa-install>Install PWA Starter</pwa-install>
+        <pwa-install>Install Ryanz Space</pwa-install>
       </div>
     `;
   }
